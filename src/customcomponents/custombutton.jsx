64fcd3dda9a8e80fb748b3e7b34fcbe2a2c50a00
Muhammad Mubeen Maroof <mubeen.maroof007@gmail.com>
2024-11-05 12:14:02 +0500

    Version Update

--- src/customcomponents/custombutton.jsx
+++ src/customcomponents/custombutton.jsx
@@ -1,33 +1,34 @@
 import { StyleSheet, TouchableOpacity, Text } from "react-native";
-import { modifiers, colors } from "../utils/theme"
-
+import { modifiers, colors } from "../utils/theme";
 
 function CusButton({
-    title = 'title',
-    onButtonPress,
-    bgColor = colors.primary }) {
-    return (
-        <TouchableOpacity style={[styles.container, { backgroundColor: bgColor }]} onPress={onButtonPress}>
-            <Text style={styles.btnText}>{title}</Text>
-        </TouchableOpacity>
-    );
+  title = "title",
+  onButtonPress,
+  bgColor = colors.primary,
+}) {
+  return (
+    <TouchableOpacity
+      style={[styles.container, { backgroundColor: bgColor }]}
+      onPress={onButtonPress}
+    >
+      <Text style={styles.btnText}>{title}</Text>
+    </TouchableOpacity>
+  );
 }
 
 export { CusButton };
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 10,
-        borderRadius: 50,
-        justifyContent: "center",
-        alignItems: "center",
-        marginVertical: modifiers.itemMargin,
-
-
-    },
-    btnText: {
-        color: colors.white,
-        fontSize: 16,
-        fontWeight: '400'
-    }
-})
+  container: {
+    padding: 10,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: modifiers.itemMargin,
+  },
+  btnText: {
+    color: colors.white,
+    fontSize: 16,
+    fontWeight: "400",
+  },
+});
